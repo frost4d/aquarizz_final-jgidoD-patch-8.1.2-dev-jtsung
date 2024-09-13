8a2ef623e85db4d3cd7aecee1a2639a20049c27b
Gideon <60163920+jgidoD@users.noreply.github.com>
2024-09-13 22:45:59 +0800

    email passwordless signin

--- src/components/main/revisionmain/components/VerifyEmail.js
+++ src/components/main/revisionmain/components/VerifyEmail.js
@@ -16,24 +16,7 @@ const VerifyEmail = (props) => {
   const verifyModal = useDisclosure();
   const [email, setEmail] = useState("");
   const [number, setNumber] = useState();
-  //   const accountSid = "AC80a5dfe1970560bc6b823ed01bbb3135";
-  //   const authToken = "e99491db92090500f6c792769cd6d3e3";
-  //   const client = twilio(accountSid, authToken);
-  //   const handleSendOtp = async (body) => {
-  //     let msgOptions = {
-  //       from: accountSid,
-  //       to: number,
-  //       body,
-  //     };
-  //     client;
-  //     try {
-  //       const message = await client.messages.create(msgOptions);
-  //       console.log(message);
-  //     } catch (err) {
-  //       console.log(err.message);
-  //     }
-  //   };
-
+git 
   return (
     <>
       <Modal isOpen={props.isOpen} onClose={props.onClose}>
